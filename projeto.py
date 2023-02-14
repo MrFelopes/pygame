edc4067c6dcf7a83e2859162e2cec9208bc74bda
MrFelopes <feliperfragoso@gmail.com>
2023-02-13 22:35:40 -0300

    apenas brincando

---
+++ projeto.py
@@ -0,0 +1,24 @@
+
+print ('Isso é um teste para o jogo.')
+
+nome = input('Digite seu nome: ')
+print ('Olá, ' + nome + '. Agora por favor selecione uma das opções:')
+
+while True:
+    try:
+        decisao1 = input('Poggers ou Noggers? ')
+
+        if (decisao1 == 'Poggers'):
+            print ('sus amogus!')
+            
+
+        elif(decisao1 == 'Noggers'):
+            print('you should kill yourself NOW!!!!!')
+            
+
+    except ValueError:
+        print('escreve certo seu merda')
+    
+
+
+            
